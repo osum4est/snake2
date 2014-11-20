6bf551fb9498c7913930946a8b1dab32509bcb90
osum4est <osumforrest@gmail.com>
2014-11-20 08:35:18 -0700

    Added mouse follower

---
+++ Snake2/Snake2/Objects/_MouseFollowerObject.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake2
+{
+    class _MouseFollowerObject : BaseGameObject, ILightable
+    {
+        public Color lightColor { get; set; }
+        public float lightStrength { get; set; }
+        public float lightRadius { get; set; }
+
+        public _MouseFollowerObject() : base() { }
+        public override void SnakeCollision()
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            position.X = Mouse.GetState().X - 16;
+            position.Y = Mouse.GetState().Y - 16;
+            base.Update(gameTime);
+        }
+
+        public override void Initialize()
+        {
+            lightColor = new Color(255, 0, 0);
+            lightStrength = .6f;
+            lightRadius = 200;
+
+            Sprite = Sprites.firefly;
+            base.Initialize();
+        }
+    }
+}
